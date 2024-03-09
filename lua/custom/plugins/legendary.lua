55db7daaebe3322c759b7970800c2bd594ae73ea
jmpaz <30947643+jmpaz@users.noreply.github.com>
2024-03-09 16:33:29 -0500

    Remove legendary.nvim

--- lua/custom/plugins/legendary.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    'mrjones2014/legendary.nvim',
-    config = function()
-      vim.keymap.set('n', '<leader>sK', '<cmd>Legendary<cr>', { desc = 'Legendary keymaps' })
-    end,
-    keys = { '<leader>sK' },
-  },
-}
--- lua/custom/plugins/smart-splits.lua
+++ lua/custom/plugins/smart-splits.lua
@@ -2,26 +2,34 @@ return {
   {
     'mrjones2014/smart-splits.nvim',
     event = 'VeryLazy',
-    build = './kitty/install-kittens.bash',
+    -- build = './kitty/install-kittens.bash',
     config = function()
-      require('smart-splits').setup()
-      require('legendary').setup {
-        extensions = {
-          smart_splits = {
-            directions = { 'h', 'j', 'k', 'l' },
-            mods = {
-              -- for moving cursor between windows
-              move = '<C>',
-              resize = '<C-M>',
-              -- for swapping window buffers
-              swap = {
-                mod = '<C>',
-                prefix = '<C-w>',
-              },
-            },
-          },
-        },
-      }
+      local smart_splits = require 'smart-splits'
+      -- resizing windows
+      vim.keymap.set('n', '<C-Left>', smart_splits.resize_left)
+      vim.keymap.set('n', '<C-Down>', smart_splits.resize_down)
+      vim.keymap.set('n', '<C-Up>', smart_splits.resize_up)
+      vim.keymap.set('n', '<C-Right>', smart_splits.resize_right)
+      -- moving between splits
+      vim.keymap.set('n', '<C-h>', smart_splits.move_cursor_left)
+      vim.keymap.set('n', '<C-j>', smart_splits.move_cursor_down)
+      vim.keymap.set('n', '<C-k>', smart_splits.move_cursor_up)
+      vim.keymap.set('n', '<C-l>', smart_splits.move_cursor_right)
+      vim.keymap.set('n', '<C-\\>', smart_splits.move_cursor_previous)
+      -- swapping buffers between windows
+      vim.keymap.set('n', '<C-M-h>', function()
+        smart_splits.swap_buf_left { move_cursor = true }
+      end)
+      vim.keymap.set('n', '<C-M-j>', function()
+        smart_splits.swap_buf_down { move_cursor = true }
+      end)
+      vim.keymap.set('n', '<C-M-k>', function()
+        smart_splits.swap_buf_up { move_cursor = true }
+      end)
+      vim.keymap.set('n', '<C-M-l>', function()
+        smart_splits.swap_buf_right { move_cursor = true }
+      end)
+      smart_splits.setup {}
     end,
   },
 }
