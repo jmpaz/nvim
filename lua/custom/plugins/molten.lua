49ab7e3274260756ea61fcf65e9e91d66b9381cb
jmpaz <30947643+jmpaz@users.noreply.github.com>
2024-03-13 15:20:37 -0400

    Add bindings for running/working with cell outputs

--- lua/custom/plugins/molten.lua
+++ lua/custom/plugins/molten.lua
@@ -1,91 +1,89 @@
 return {
   {
-    "benlubas/molten-nvim",
-    version = "<2.0.0",
-    build = ":UpdateRemotePlugins",
+    'benlubas/molten-nvim',
+    version = '<2.0.0',
+    build = ':UpdateRemotePlugins',
     init = function()
       -- molten-nvim keybindings
-      local wk = require("which-key")
+      local wk = require 'which-key'
 
       -- Setting global variables for molten
       vim.g.molten_output_win_max_height = 20
 
       -- Key bindings setup
-      vim.keymap.set("n", "<localleader>mki", ":MoltenInit<CR>", { silent = true })
-      vim.keymap.set("n", "<localleader>mkd", ":MoltenDeinit<CR>", { silent = true })
-      vim.keymap.set("n", "<localleader>mkr", ":MoltenRestart<CR>", { silent = true })
-      vim.keymap.set("n", "<localleader>mks", ":MoltenInterrupt<CR>", { silent = true })
+      vim.keymap.set('n', '<localleader>mki', ':MoltenInit<CR>', { silent = true })
+      vim.keymap.set('n', '<localleader>mkd', ':MoltenDeinit<CR>', { silent = true })
+      vim.keymap.set('n', '<localleader>mkr', ':MoltenRestart<CR>', { silent = true })
+      vim.keymap.set('n', '<localleader>mks', ':MoltenInterrupt<CR>', { silent = true })
 
-      vim.keymap.set("n", "<localleader>mcg", ":MoltenGoto<CR>", { silent = true })
-      vim.keymap.set("n", "<localleader>mcn", ":MoltenNext<CR>", { silent = true })
-      vim.keymap.set("n", "<localleader>mcp", ":MoltenPrev<CR>", { silent = true })
-      vim.keymap.set("n", "<localleader>mcd", ":MoltenDelete<CR>", { silent = true })
-      vim.keymap.set("n", "<localleader>mcr", ":MoltenReevaluateCell<CR>", { silent = true })
+      vim.keymap.set('n', '<localleader>mcg', ':MoltenGoto<CR>', { silent = true })
+      vim.keymap.set('n', '<localleader>mcn', ':MoltenNext<CR>', { silent = true })
+      vim.keymap.set('n', '<localleader>mcp', ':MoltenPrev<CR>', { silent = true })
+      vim.keymap.set('n', '<localleader>mcd', ':MoltenDelete<CR>', { silent = true })
+      vim.keymap.set('n', '<localleader>mcr', ':MoltenReevaluateCell<CR>', { silent = true })
 
-      vim.keymap.set("n", "<localleader>mrl", ":MoltenEvaluateLine<CR>", { silent = true })
-      vim.keymap.set("n", "<localleader>mro", ":MoltenEvaluateOperator<CR>", { silent = true })
-      vim.keymap.set("n", "<localleader>mra", ":MoltenEvaluateArgument<CR>", { silent = true })
+      vim.keymap.set('n', '<localleader>mrl', ':MoltenEvaluateLine<CR>', { silent = true })
+      vim.keymap.set('n', '<localleader>mro', ':MoltenEvaluateOperator<CR>', { silent = true })
+      vim.keymap.set('n', '<localleader>mra', ':MoltenEvaluateArgument<CR>', { silent = true })
 
-      vim.keymap.set("n", "<localleader>mos", ":MoltenShowOutput<CR>", { silent = true })
-      vim.keymap.set("n", "<localleader>moh", ":MoltenHideOutput<CR>", { silent = true })
-      vim.keymap.set("n", "<localleader>moe", ":noautocmd MoltenEnterOutput<CR>", { silent = true })
-      vim.keymap.set("n", "<localleader>moo", ":MoltenOpenInBrowser<CR>", { silent = true })
+      vim.keymap.set('n', '<localleader>ms', ':MoltenShowOutput<CR>', { silent = true, desc = 'Show Output' })
+      vim.keymap.set('n', '<localleader>mh', ':MoltenHideOutput<CR>', { silent = true, desc = 'Hide Output' })
+      vim.keymap.set('n', '<localleader>me', ':noautocmd MoltenEnterOutput<CR>', { silent = true, desc = 'Enter Output' })
+      vim.keymap.set('n', '<localleader>mos', ':MoltenShowOutput<CR>', { silent = true })
+      vim.keymap.set('n', '<localleader>moh', ':MoltenHideOutput<CR>', { silent = true })
+      vim.keymap.set('n', '<localleader>moe', ':noautocmd MoltenEnterOutput<CR>', { silent = true })
+      vim.keymap.set('n', '<localleader>moo', ':MoltenOpenInBrowser<CR>', { silent = true })
 
-      vim.keymap.set("n", "<localleader>mfs", ":MoltenSave<CR>", { silent = true })
-      vim.keymap.set("n", "<localleader>mfl", ":MoltenLoad<CR>", { silent = true })
-      vim.keymap.set("n", "<localleader>mfe", ":MoltenExportOutput<CR>", { silent = true })
-      vim.keymap.set("n", "<localleader>mfi", ":MoltenImportOutput<CR>", { silent = true })
+      vim.keymap.set('n', '<localleader>mfs', ':MoltenSave<CR>', { silent = true })
+      vim.keymap.set('n', '<localleader>mfl', ':MoltenLoad<CR>', { silent = true })
+      vim.keymap.set('n', '<localleader>mfe', ':MoltenExportOutput<CR>', { silent = true })
+      vim.keymap.set('n', '<localleader>mfi', ':MoltenImportOutput<CR>', { silent = true })
 
-      vim.keymap.set("n", "<localleader>mi", ":MoltenInfo<CR>", { silent = true })
-      vim.keymap.set(
-        "v",
-        "<localleader>m",
-        ":<C-u>MoltenEvaluateVisual<CR>",
-        { silent = true, desc = "Evaluate selection (Molten)" }
-      )
+      vim.keymap.set('n', '<localleader>mi', ':MoltenInfo<CR>', { silent = true })
+      vim.keymap.set('v', '<localleader>m', ':<C-u>MoltenEvaluateVisual<CR>', { silent = true, desc = 'Evaluate selection (Molten)' })
 
       -- which-key menu definition
       wk.register({
         m = {
-          name = "Molten",
+          name = 'Molten',
           k = {
-            name = "Kernel",
-            i = "Initialize Kernel",
-            d = "De-initialize Kernel",
-            r = "Restart Kernel",
-            s = "Interrupt Kernel",
+            name = 'Kernel',
+            i = 'Initialize Kernel',
+            d = 'De-initialize Kernel',
+            r = 'Restart Kernel',
+            s = 'Interrupt Kernel',
           },
           c = {
-            name = "Cells",
-            g = "Go to Cell",
-            n = "Next Cell",
-            p = "Previous Cell",
-            d = "Delete Cell",
-            r = "Re-evaluate Cell",
+            name = 'Cells',
+            g = 'Go to Cell',
+            n = 'Next Cell',
+            p = 'Previous Cell',
+            d = 'Delete Cell',
+            r = 'Re-evaluate Cell',
           },
           r = {
-            name = "Evaluate",
-            l = "Evaluate Line",
-            o = "Evaluate Operator",
-            a = "Evaluate Argument",
+            name = 'Evaluate',
+            l = 'Evaluate Line',
+            o = 'Evaluate Operator',
+            a = 'Evaluate Argument',
           },
           o = {
-            name = "Output",
-            s = "Show Output Window",
-            h = "Hide Output Window",
-            e = "Enter Output Window",
-            o = "Open Output in Browser",
+            name = 'Output',
+            s = 'Show Output Window',
+            h = 'Hide Output Window',
+            e = 'Enter Output Window',
+            o = 'Open Output in Browser',
           },
           f = {
-            name = "File",
-            s = "Save Cells",
-            l = "Load Cells",
-            e = "Export to .ipynb",
-            i = "Import from .ipynb",
+            name = 'File',
+            s = 'Save Cells',
+            l = 'Load Cells',
+            e = 'Export to .ipynb',
+            i = 'Import from .ipynb',
           },
-          i = "Info",
+          i = 'Info',
         },
-      }, { prefix = "<localleader>" })
+      }, { prefix = '<localleader>' })
     end,
   },
 }
--- lua/custom/plugins/quarto.lua
+++ lua/custom/plugins/quarto.lua
@@ -1,13 +1,13 @@
 return {
   {
-    "quarto-dev/quarto-nvim",
+    'quarto-dev/quarto-nvim',
     opts = {
       lspFeatures = {
-        languages = { "python", "bash", "html", "lua" },
-        chunks = "all",
+        languages = { 'python', 'bash', 'html', 'lua' },
+        chunks = 'all',
         diagnostics = {
           enabled = true,
-          triggers = { "BufWritePost" },
+          triggers = { 'BufWritePost' },
         },
         completion = {
           enabled = true,
@@ -15,35 +15,37 @@ return {
       },
       codeRunner = {
         enabled = true,
-        default_method = "molten",
-        never_run = { "yaml" },
+        default_method = 'molten',
+        never_run = { 'yaml' },
       },
     },
     init = function()
-      local runner = require("quarto.runner")
+      local runner = require 'quarto.runner'
 
       -- bindings
-      vim.keymap.set("n", "<localleader>rc", runner.run_cell, { silent = true })
-      vim.keymap.set("n", "<localleader>ra", runner.run_above, { silent = true })
-      vim.keymap.set("n", "<localleader>rA", runner.run_all, { silent = true })
-      vim.keymap.set("n", "<localleader>rl", runner.run_line, { silent = true })
-      vim.keymap.set("v", "<localleader>r", runner.run_range, { silent = true, desc = "Evaluate selection (Quarto)" })
+      vim.keymap.set('n', '<C-M-r>', runner.run_cell, { silent = true })
+      vim.keymap.set('v', '<C-M-r>', runner.run_range, { silent = true })
+      vim.keymap.set('n', '<localleader>rc', runner.run_cell, { silent = true })
+      vim.keymap.set('n', '<localleader>ra', runner.run_above, { silent = true })
+      vim.keymap.set('n', '<localleader>rA', runner.run_all, { silent = true })
+      vim.keymap.set('n', '<localleader>rl', runner.run_line, { silent = true })
+      vim.keymap.set('v', '<localleader>r', runner.run_range, { silent = true, desc = 'Evaluate selection (Quarto)' })
 
       -- which-key
-      local wk = require("which-key")
+      local wk = require 'which-key'
       wk.register({
         r = {
-          name = "Run (Quarto)",
-          c = "Run Cell",
-          a = "Run Cell + Above",
-          A = "Run All Cells",
-          l = "Run Line",
+          name = 'Run (Quarto)',
+          c = 'Run Cell',
+          a = 'Run Cell + Above',
+          A = 'Run All Cells',
+          l = 'Run Line',
         },
-      }, { prefix = "<localleader>" })
+      }, { prefix = '<localleader>' })
     end,
   },
   {
-    "jmbuhr/otter.nvim",
+    'jmbuhr/otter.nvim',
     opts = {
       buffers = {
         set_filetype = true,
