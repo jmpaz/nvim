00547702322a617c51adb9c831d03b538ece4bfc
jmpaz <30947643+jmpaz@users.noreply.github.com>
2024-04-01 19:38:45 -0400

    Add telescope extensions

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -37,4 +37,39 @@ return {
       require('telescope').load_extension 'emoji'
     end,
   },
+  {
+    'MaximilianLloyd/adjacent.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim' },
+    config = function()
+      require('telescope').load_extension 'adjacent'
+      vim.keymap.set('n', '<leader>sa', '<cmd>Telescope adjacent<CR>', { noremap = true, silent = false, desc = '[S]earch [A]djacent' })
+    end,
+  },
+  {
+    'jvgrootveld/telescope-zoxide',
+    dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim', 'nvim-lua/popup.nvim' },
+    config = function()
+      require('telescope').load_extension 'zoxide'
+      vim.keymap.set('n', '<leader>sz', '<cmd>Telescope zoxide list<CR>', { noremap = true, silent = false, desc = '[S]earch [Z]oxide' })
+    end,
+  },
+  {
+    'paopaol/telescope-git-diffs.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim' },
+    config = function()
+      require('telescope').load_extension 'git_diffs'
+      vim.keymap.set(
+        'n',
+        '<leader>gd',
+        '<cmd>lua require("telescope").extensions.git_diffs.diff_commits()<CR>',
+        { noremap = true, silent = false, desc = '[G]it [D]iffs' }
+      )
+      vim.keymap.set(
+        'n',
+        '<leader>gc',
+        '<cmd>lua require("telescope").extensions.git_diffs.diff_commits()<CR>',
+        { noremap = true, silent = false, desc = '[G]it [C]ommits' }
+      )
+    end,
+  },
 }
