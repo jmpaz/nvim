53ff356716bfa8aad5032736159bc988af99cbd9
jmpaz <30947643+jmpaz@users.noreply.github.com>
2024-05-02 16:40:56 -0400

    Add zellij-nav.nvim

--- lua/custom/plugins/splits.lua
+++ lua/custom/plugins/splits.lua
@@ -13,10 +13,10 @@ return {
       -- resizing windows
       vim.keymap.set('n', '<C-w>r', smart_splits.start_resize_mode)
       -- moving between splits
-      vim.keymap.set('n', '<C-h>', smart_splits.move_cursor_left)
-      vim.keymap.set('n', '<C-j>', smart_splits.move_cursor_down)
-      vim.keymap.set('n', '<C-k>', smart_splits.move_cursor_up)
-      vim.keymap.set('n', '<C-l>', smart_splits.move_cursor_right)
+      -- vim.keymap.set('n', '<C-h>', smart_splits.move_cursor_left)
+      -- vim.keymap.set('n', '<C-j>', smart_splits.move_cursor_down)
+      -- vim.keymap.set('n', '<C-k>', smart_splits.move_cursor_up)
+      -- vim.keymap.set('n', '<C-l>', smart_splits.move_cursor_right)
       vim.keymap.set('n', '<C-\\>', smart_splits.move_cursor_previous)
       -- swapping buffers between windows
       vim.keymap.set('n', '<C-M-h>', function()
@@ -33,5 +33,17 @@ return {
       end)
       smart_splits.setup {}
     end,
+  },
+  {
+    'https://git.sr.ht/~swaits/zellij-nav.nvim',
+    lazy = true,
+    event = 'VeryLazy',
+    keys = {
+      { '<c-h>', '<cmd>ZellijNavigateLeft<cr>', { silent = true, desc = 'navigate left' } },
+      { '<c-j>', '<cmd>ZellijNavigateDown<cr>', { silent = true, desc = 'navigate down' } },
+      { '<c-k>', '<cmd>ZellijNavigateUp<cr>', { silent = true, desc = 'navigate up' } },
+      { '<c-l>', '<cmd>ZellijNavigateRight<cr>', { silent = true, desc = 'navigate right' } },
+    },
+    opts = {},
   },
 }
