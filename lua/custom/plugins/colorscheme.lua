37b2ed0aea8313922d7cd407deb2a79de24667c0
jmpaz <30947643+jmpaz@users.noreply.github.com>
2024-10-03 22:33:31 -0400

    Switch from rose-pine to neopywal.nvim

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,45 +1,17 @@
 return {
-  -- {
-  --   'folke/tokyonight.nvim',
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     vim.cmd.colorscheme 'tokyonight-storm'
-  --   end,
-  -- },
   {
-    'rose-pine/neovim',
-    name = 'rose-pine',
+    'RedsXDD/neopywal.nvim',
+    name = 'neopywal',
     lazy = false,
     priority = 1000,
+    opts = {},
     config = function()
-      require('rose-pine').setup {
-        variant = 'auto',
-        dark_variant = 'moon',
-        dim_inactive_windows = false,
-        extend_background_behind_borders = true,
-
-        styles = {
-          bold = true,
-          italic = true,
-          transparency = true,
-        },
+      require('neopywal').setup {
+        use_wallust = true,
       }
-      vim.cmd.colorscheme 'rose-pine'
+      vim.o.background = 'dark'
+      vim.cmd.colorscheme 'neopywal'
     end,
   },
-  -- {
-  --   "oncomouse/lushwal.nvim",
-  --   cmd = { "LushwalCompile" },
-  --   dependencies = {
-  --     { "rktjmp/lush.nvim" },
-  --     { "rktjmp/shipwright.nvim" },
-  --   },
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     vim.cmd.colorscheme 'lushwal'
-  --   end,
-  -- },
 }
 -- vim: ts=2 sts=2 sw=2 et
