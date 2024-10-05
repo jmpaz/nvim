10aecbaab63db88809e4a8e7f234b0169c3485e8
jmpaz <30947643+jmpaz@users.noreply.github.com>
2024-10-05 13:05:37 -0400

    Update splits.lua
    Enable navigating with ctrl-direction
    Add vim-tmux-navigator

--- lua/custom/plugins/splits.lua
+++ lua/custom/plugins/splits.lua
@@ -2,7 +2,6 @@ return {
   {
     'mrjones2014/smart-splits.nvim',
     event = 'VeryLazy',
-    -- build = './kitty/install-kittens.bash',
     resize_mode = {
       quit_key = '<ESC>',
       resize_keys = { 'h', 'j', 'k', 'l' },
@@ -12,12 +11,14 @@ return {
       local smart_splits = require 'smart-splits'
       -- resizing windows
       vim.keymap.set('n', '<C-w>r', smart_splits.start_resize_mode)
+      --
       -- moving between splits
-      -- vim.keymap.set('n', '<C-h>', smart_splits.move_cursor_left)
-      -- vim.keymap.set('n', '<C-j>', smart_splits.move_cursor_down)
-      -- vim.keymap.set('n', '<C-k>', smart_splits.move_cursor_up)
-      -- vim.keymap.set('n', '<C-l>', smart_splits.move_cursor_right)
       vim.keymap.set('n', '<C-\\>', smart_splits.move_cursor_previous)
+      vim.keymap.set('n', '<C-h>', smart_splits.move_cursor_left)
+      vim.keymap.set('n', '<C-j>', smart_splits.move_cursor_down)
+      vim.keymap.set('n', '<C-k>', smart_splits.move_cursor_up)
+      vim.keymap.set('n', '<C-l>', smart_splits.move_cursor_right)
+      --
       -- swapping buffers between windows
       vim.keymap.set('n', '<C-M-h>', function()
         smart_splits.swap_buf_left { move_cursor = true }
@@ -31,19 +32,42 @@ return {
       vim.keymap.set('n', '<C-M-l>', function()
         smart_splits.swap_buf_right { move_cursor = true }
       end)
+
       smart_splits.setup {}
     end,
   },
-  {
-    'https://git.sr.ht/~swaits/zellij-nav.nvim',
-    lazy = true,
-    event = 'VeryLazy',
-    keys = {
-      { '<c-h>', '<cmd>ZellijNavigateLeft<cr>', { silent = true, desc = 'navigate left' } },
-      { '<c-j>', '<cmd>ZellijNavigateDown<cr>', { silent = true, desc = 'navigate down' } },
-      { '<c-k>', '<cmd>ZellijNavigateUp<cr>', { silent = true, desc = 'navigate up' } },
-      { '<c-l>', '<cmd>ZellijNavigateRight<cr>', { silent = true, desc = 'navigate right' } },
-    },
-    opts = {},
-  },
+  (function()
+    if vim.loop.os_uname().sysname == 'Darwin' then
+      return {
+        'https://git.sr.ht/~swaits/zellij-nav.nvim',
+        lazy = true,
+        event = 'VeryLazy',
+        keys = {
+          { '<c-h>', '<cmd>ZellijNavigateLeft<cr>', { silent = true, desc = 'navigate left' } },
+          { '<c-j>', '<cmd>ZellijNavigateDown<cr>', { silent = true, desc = 'navigate down' } },
+          { '<c-k>', '<cmd>ZellijNavigateUp<cr>', { silent = true, desc = 'navigate up' } },
+          { '<c-l>', '<cmd>ZellijNavigateRight<cr>', { silent = true, desc = 'navigate right' } },
+        },
+        opts = {},
+      }
+    else
+      return {
+        'christoomey/vim-tmux-navigator',
+        cmd = {
+          'TmuxNavigateLeft',
+          'TmuxNavigateDown',
+          'TmuxNavigateUp',
+          'TmuxNavigateRight',
+          'TmuxNavigatePrevious',
+        },
+        keys = {
+          { '<m-h>', '<cmd>TmuxNavigateLeft<cr>' },
+          { '<m-j>', '<cmd>TmuxNavigateDown<cr>' },
+          { '<m-k>', '<cmd>TmuxNavigateUp<cr>' },
+          { '<m-l>', '<cmd>TmuxNavigateRight<cr>' },
+          { '<m-\\>', '<cmd>TmuxNavigatePrevious<cr>' },
+        },
+      }
+    end
+  end)(),
 }
