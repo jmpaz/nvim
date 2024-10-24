516151091e16ec521c21345c8f2f50e318eb03da
jmpaz <30947643+jmpaz@users.noreply.github.com>
2024-10-24 03:38:02 -0400

    Add tab bindings

--- init.lua
+++ init.lua
@@ -315,17 +315,25 @@ later(function()
 end)
 
 --
--- tabs
+-- tabs/buffers
 later(function()
-  -- Move between buffers
   vim.keymap.set('n', '<S-h>', ':bprevious<CR>', { noremap = true, silent = true, desc = 'previous' })
   vim.keymap.set('n', '<S-l>', ':bnext<CR>', { noremap = true, silent = true, desc = 'next' })
 
-  -- Leader key bindings for tab management
   vim.keymap.set('n', '<Space>tn', ':tabnext<CR>', { noremap = true, silent = true, desc = 'next' })
   vim.keymap.set('n', '<Space>tp', ':tabprevious<CR>', { noremap = true, silent = true, desc = 'prev' })
   vim.keymap.set('n', '<Space>tc', ':tabnew<CR>', { noremap = true, silent = true, desc = 'create' })
   vim.keymap.set('n', '<Space>tx', ':tabclose<CR>', { noremap = true, silent = true, desc = 'close' })
+
+  vim.keymap.set('n', '<Space>tH', ':tabmove -1<CR>', { noremap = true, silent = true, desc = 'move left' })
+  vim.keymap.set('n', '<Space>tL', ':tabmove +1<CR>', { noremap = true, silent = true, desc = 'move right' })
+  vim.keymap.set('n', '<Space>t1', '1gt', { noremap = true, silent = true, desc = 'goto 1' })
+  vim.keymap.set('n', '<Space>t2', '2gt', { noremap = true, silent = true, desc = 'goto 2' })
+  vim.keymap.set('n', '<Space>t3', '3gt', { noremap = true, silent = true, desc = 'goto 3' })
+  vim.keymap.set('n', '<Space>t4', '4gt', { noremap = true, silent = true, desc = 'goto 4' })
+  vim.keymap.set('n', '<Space>t5', '5gt', { noremap = true, silent = true, desc = 'goto 5' })
+  vim.keymap.set('n', '<Space>tl', 'g$', { noremap = true, silent = true, desc = 'last' })
+  vim.keymap.set('n', '<Space>tf', ':tabfirst<CR>', { noremap = true, silent = true, desc = 'first' })
 end)
 
 --
