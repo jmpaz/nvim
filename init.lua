d2860cc1080c1fa2b21da5e0ee7c22eea41ffb15
jmpaz <30947643+jmpaz@users.noreply.github.com>
2025-03-04 16:44:59 -0500

    Fix markdown indentation

--- init.lua
+++ init.lua
@@ -370,10 +370,6 @@ now(function()
   vim.o.list = true
   vim.o.listchars = table.concat({ 'extends:…', 'nbsp:␣', 'precedes:…', 'tab:> ' }, ',')
   vim.o.autoindent = true
-  vim.o.shiftwidth = 2
-  vim.o.tabstop = 2
-  vim.o.softtabstop = 2
-  vim.o.expandtab = true
   vim.o.scrolloff = 10
   vim.o.updatetime = 1000
   vim.opt.iskeyword:append('-')
@@ -386,6 +382,21 @@ end)
 
 -- indent
 now(function()
+  vim.o.shiftwidth = 2
+  vim.o.tabstop = 2
+  vim.o.softtabstop = 2
+  vim.o.expandtab = true
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "markdown" },
+    callback = function()
+      vim.opt_local.shiftwidth = 2
+      vim.opt_local.tabstop = 2
+      vim.opt_local.softtabstop = 2
+      vim.opt_local.expandtab = true
+    end,
+  })
+
   vim.api.nvim_create_autocmd("FileType", {
     group = group,
     pattern = { "python" },
