8434e33cfe802a334d8df63750c41a4ff0beeb63
jmpaz <30947643+jmpaz@users.noreply.github.com>
2025-06-15 15:10:56 -0400

    Add neocodeium

---
+++ lua/plugins/neocodeium.lua
@@ -0,0 +1,37 @@
+local M = {}
+
+function M.setup()
+  local later, add = MiniDeps.later, MiniDeps.add
+  later(function()
+    add({
+      source = 'monkoose/neocodeium',
+    })
+
+    local neocodeium = require('neocodeium')
+    local blink = require('blink.cmp')
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'BlinkCmpMenuOpen',
+      callback = function() neocodeium.clear() end,
+    })
+
+    neocodeium.setup({
+      filter = function() return not blink.is_visible() end,
+      show_label = true,
+      silent = true,
+      filetypes = {},
+    })
+
+    vim.keymap.set('i', '<A-CR>', neocodeium.accept)
+
+    vim.keymap.set('i', '<A-w>', neocodeium.accept_word)
+    vim.keymap.set('i', '<A-e>', neocodeium.accept_line)
+
+    vim.keymap.set('i', '<A-r>', function() neocodeium.cycle_or_complete() end)
+    vim.keymap.set('i', '<A-t>', function() neocodeium.cycle_or_complete(-1) end)
+
+    vim.keymap.set('i', '<A-c>', neocodeium.clear)
+  end)
+end
+
+return M
